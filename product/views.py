53e27081be2471f9132cab293edc200fc2eb6d5c
tilek075 <tomurbekov@gmail.com>
2023-04-01 21:26:25 +0600

    hw1

---
+++ product/views.py
@@ -0,0 +1,59 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from product.serializers import ProductSerializer, CategorySerializer, ReviewSerializer
+from product.models import Product, Category, Review
+from rest_framework import status
+
+
+@api_view(['GET'])
+def product_list_api_view(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def product_detail_api_view(request, id):
+    try:
+        product = Product.objects.get(id=id)
+    except Product.DoesNotExist:
+        return Response(data={'error': 'Product not found!'},
+                        status=status.HTTP_404_NOT_FOUND)
+    serializer = ProductSerializer(product, many=False)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def category_list_api_view(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def category_detail_api_view(request, id):
+    try:
+        category = Category.objects.get(id=id)
+    except Category.DoesNotExist:
+        return Response(data={'error': 'Category not found!'},
+                        status=status.HTTP_404_NOT_FOUND)
+    serializer = CategorySerializer(category, many=False)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def review_list_api_view(request):
+    reviews = Review.objects.all()
+    serializer = ReviewSerializer(reviews, many=True)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def review_detail_api_view(request, id):
+    try:
+        review = Product.objects.get(id=id)
+    except Review.DoesNotExist:
+        return Response(data={'error': 'Review not found'},
+                        status=status.HTTP_404_NOT_FOUND)
+    serializer = ProductSerializer(review, many=False)
+    return Response(data=serializer.data)
